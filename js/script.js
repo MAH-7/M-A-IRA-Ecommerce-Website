25e9b1db766d439e6a0e271482c4d69daa81ac0a
emanhakim <97828413+emanhakim@users.noreply.github.com>
2023-11-09 11:33:40 +0800

    Update-Countdown

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+function updateTimer() {
+  const endDate = new Date("2023-12-31 23:59:59").getTime(); // Set your flash sale end date here
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
+  const now = new Date().getTime();
+  const timeRemaining = endDate - now;
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("days").textContent = String(days).padStart(2, "0");
+  document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+  document.getElementById("minutes").textContent = String(minutes).padStart(
+    2,
+    "0"
+  );
+  document.getElementById("seconds").textContent = String(seconds).padStart(
+    2,
+    "0"
+  );
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+  if (timeRemaining <= 0) {
+    clearInterval(timerInterval);
+    document.querySelector(".timer").innerHTML = "Sale has ended!";
   }
-}, 1000);
+}
+
+updateTimer(); // Initial call
+const timerInterval = setInterval(updateTimer, 1000); // Update the timer every second
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
