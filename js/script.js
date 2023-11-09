55a312acde690eb1abc0f6ac90c06645802a0f73
emanhakim <97828413+emanhakim@users.noreply.github.com>
2023-11-09 11:29:18 +0800

    Revert "Update-Flash Sale > Countdown"
    This reverts commit 043753f56caf75ab0468ee90299af498fb77b0b6.

--- js/script.js
+++ js/script.js
@@ -26,39 +26,6 @@ var x = setInterval(function () {
   }
 }, 1000);
 
-function updateTimer() {
-  const endDate = new Date("2023-12-31 23:59:59").getTime(); // Set your flash sale end date here
-
-  const now = new Date().getTime();
-  const timeRemaining = endDate - now;
-
-  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-  document.getElementById("days").textContent = String(days).padStart(2, "0");
-  document.getElementById("hours").textContent = String(hours).padStart(2, "0");
-  document.getElementById("minutes").textContent = String(minutes).padStart(
-    2,
-    "0"
-  );
-  document.getElementById("seconds").textContent = String(seconds).padStart(
-    2,
-    "0"
-  );
-
-  if (timeRemaining <= 0) {
-    clearInterval(timerInterval);
-    document.querySelector(".timer").innerHTML = "Sale has ended!";
-  }
-}
-
-updateTimer(); // Initial call
-const timerInterval = setInterval(updateTimer, 1000); // Update the timer every second
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 30,
