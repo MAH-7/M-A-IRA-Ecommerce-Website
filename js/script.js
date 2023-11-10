e814608a0dd6327fac3bd707c4c043fd968a960e
emanhakim <97828413+emanhakim@users.noreply.github.com>
2023-11-11 03:21:25 +0800

    Update-Swiper

--- js/script.js
+++ js/script.js
@@ -32,10 +32,24 @@ updateTimer(); // Initial call
 const timerInterval = setInterval(updateTimer, 1000); // Update the timer every second
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
+  slidesPerView: 2,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
   },
 });
